def4898df2d8787b864f9a7292289ef0e4958a06
joeunhye <masimel33@gmail.com>
2023-10-07 13:20:01 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Gallery from './components/sub/gallery/Gallery';
 import Members from './components/sub/members/Members';
 import Youtube from './components/sub/youtube/Youtube';
 import './styles/Global.scss';
+import { Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Visual />
-			<Department />
-			<Gallery />
-			<Youtube />
-			<Members />
-			<Contact />
+
+			<Route exact path='/' component={Visual} />
+			<Route path='/department' component={Department} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/members' component={Members} />
+			<Route path='/contact' component={Contact} />
+
 			<Footer />
 		</>
 	);
