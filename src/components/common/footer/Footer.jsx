19a6271ed46fbe463c64a92cf524e377b83bf65a
joeunhye <masimel33@gmail.com>
2023-10-07 14:44:22 +0900

    푸터 스타일링

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import { FaYoutube, FaTwitter } from 'react-icons/fa6';
+import './Footer.scss';
+
 export default function Footer() {
 	return (
 		<footer>
-			<h1>Footer</h1>
+			<h1>DecodeLab</h1>
+			<p>2023 Decodelab &copy; All Rights Reserved.</p>
+
+			<ul>
+				<li>
+					<Link to='/'>
+						<FaYoutube size={20} />
+					</Link>
+				</li>
+				<li>
+					<Link to='/'>
+						<FaTwitter size={20} />
+					</Link>
+				</li>
+			</ul>
 		</footer>
 	);
 }
