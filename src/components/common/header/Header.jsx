1446d8720920c1426b6e8d36f87b40b22532dee0
joeunhye <masimel33@gmail.com>
2023-10-07 13:41:52 +0900

    라우터 메뉴 활성화

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,15 +1,38 @@
 import './Header.scss';
+import { NavLink, Link } from 'react-router-dom';
 
 export default function Header() {
 	return (
 		<header>
-			<h1>DECODELAB</h1>
+			<h1>
+				<Link to='/'>DECODELAB</Link>
+			</h1>
 			<ul>
-				<li>Department</li>
-				<li>Gallery</li>
-				<li>Youtube</li>
-				<li>Members</li>
-				<li>Contact</li>
+				<li>
+					<NavLink to='/department' activeClassName={'on'}>
+						Department
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='gallery' activeClassName={'on'}>
+						Gallery
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='youtube' activeClassName={'on'}>
+						Youtube
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='members' activeClassName={'on'}>
+						Members
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='contact' activeClassName={'on'}>
+						Contact
+					</NavLink>
+				</li>
 			</ul>
 		</header>
 	);
