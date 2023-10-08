4382dae07e72995a37fd0fd336eaddf1945a10f4
joeunhye <masimel33@gmail.com>
2023-10-08 15:18:48 +0900

    추가 파라미터 설정

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout({ title, children }) {
 	const splitText = useSplitText();
 
 	useEffect(() => {
-		splitText(refTitle);
+		splitText(refTitle, 0.1, 2);
 		setTimeout(() => {
 			refFrame.current.classList.add('on');
 		}, 300);
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -1,11 +1,11 @@
 export function useSplitText() {
-	return (ref) => {
+	return (ref, interval = 0, delay = 0) => {
 		let text = ref.current.innerText;
 		let tags = '';
 		let count = 0;
 
 		for (let letter of text) {
-			tags += `<span style='display:inline-block; transition-delay:${0.1 * count}s'>${letter}</span>`;
+			tags += `<span style='display:inline-block; transition-delay:${interval * count + delay}s'>${letter}</span>`;
 			count++;
 		}
 		ref.current.innerHTML = tags;
