27a1887d8c378e96de2dc913322c41daa8c3a365
joeunhye <masimel33@gmail.com>
2023-10-08 12:59:46 +0900

    리액트애니메이션 예제1

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,5 +1,25 @@
+import { useState, useRef } from 'react';
 import Layout from '../../common/layout/Layout';
+import './Department.scss';
 
 export default function Department() {
-	return <Layout title={'Department'}></Layout>;
+	const rotate = useRef(0);
+	let [num, setNum] = useState(0);
+	const plus = () => {
+		setNum(++num);
+	};
+	const minus = () => {
+		setNum(--num);
+	};
+	return (
+		<Layout title={'Department'}>
+			<button onClick={plus}>plus</button>
+			<button onClick={minus}>minus</button>
+			<article
+				style={{
+					transform: `rotate(${45 * num}deg)`,
+				}}
+			></article>
+		</Layout>
+	);
 }
