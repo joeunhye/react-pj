106af42676a9a7cf02190f0bd5ac68be53451fa3
joeunhye <masimel33@gmail.com>
2023-10-08 14:13:22 +0900

    간단한 팝업 제어

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,5 +1,27 @@
 import './Modal.scss';
+import { useEffect, useState } from 'react';
 
 export default function Modal() {
-	return <aside className='modal'>Modal</aside>;
+	const [num, setNum] = useState(0);
+	useEffect(() => {
+		console.log('컴포넌트 마운트 시 호출');
+		document.body.style.overflow = 'hidden';
+
+		return () => {
+			console.log('컴포넌트 언마운트 시 호출');
+			document.body.style.overflow = 'auto';
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log('num 변경 시 호출');
+	}, [num]);
+	return (
+		<aside className='modal'>
+			<button onClick={() => setNum(num - 1)}>Minus</button>
+			<button onClick={() => setNum(num + 1)}>Plus</button>
+
+			<h1>{num}</h1>
+		</aside>
+	);
 }
