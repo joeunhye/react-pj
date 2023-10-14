30214ef7f37b564a1b6f57c0fc2bf5e7077b7047
joeunhye <masimel33@gmail.com>
2023-10-14 15:12:04 +0900

    유튜브  api 연동

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 
 export default function Youtube() {
-	return <Layout title='Youtube'></Layout>;
+	const [vids, setVids] = useState([]);
+
+	console.log(vids);
+
+	useEffect(() => {
+		const api_key = 'AIzaSyBdfJCY-Cpvxi_gWvGVpnxqWLU4cdowO_o';
+		const pid = 'PLQYyKtRo0VeHDcXQenJwEqbY8hMmNIhjV';
+		const num = 10;
+		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+		fetch(baseURL)
+			.then((data) => data.json())
+			.then((json) => setVids(json.items));
+	}, []);
+	return (
+		<Layout title='Youtube'>
+			{vids.map((data, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{data.snippet.title}</h2>
+						<p>{data.snippet.description}</p>
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.standard.url} alt='' />
+						</div>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,8 +3,8 @@ export function useFetch() {
 		fetch(url)
 			.then((data) => data.json())
 			.then((json) => {
-				setkey && setkey(Object.keys(json)[0]);
 				setValue && setValue(Object.values(json)[0]);
+				setkey && setkey(Object.keys(json)[0]);
 			});
 	};
 }
