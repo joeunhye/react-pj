26841b09744780c0726ab27bdb979db240c11ffe
joeunhye <masimel33@gmail.com>
2023-10-14 12:56:24 +0900

    외부 데이터 패치

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,15 +1,14 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Department.scss';
-import Modal from '../../common/modal/Modal';
 
 export default function Department() {
-	const [open, setOpen] = useState(false);
-
-	return (
-		<Layout title={'Department'}>
-			<button onClick={() => setOpen(!open)}>{open ? 'CLOSE' : 'OPEN'}</button>
-			{open && <Modal />}
-		</Layout>
-	);
+	const [department, setDepartment] = useState([]);
+	console.log(department);
+	useEffect(() => {
+		fetch(`${process.env.PUBLIC_URL}/DB/department.json`)
+			.then((data) => data.json())
+			.then((json) => setDepartment(json.members));
+	}, []);
+	return <Layout title={'Department'}></Layout>;
 }
