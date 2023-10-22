f5f1a2eed42568caa45b6eeb5aca6fb5800294a5
eunhye <masimel33@gmail.com>
2023-10-22 12:50:46 +0900

    갤러리 반응형 수정

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,4 +1,5 @@
 import Layout from '../../common/layout/Layout';
+import Masonry from 'react-masonry-component';
 import './Gallery.scss';
 import { useState, useEffect } from 'react';
 
@@ -20,25 +21,27 @@ export default function Gallery() {
 		fetchGallery();
 	}, []);
 
-	console.log(Pics);
-
 	return (
 		<Layout title={'Gallery'}>
 			<div className='frame'>
-				{Pics.map((pic, idx) => {
-					return (
-						<article key={idx}>
-							<h2>{pic.title}</h2>
-							<div className='pic'>
-								<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
-							</div>
-							<div className='profile'>
-								<img src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`} alt={pic.owner} />
-								<span>{pic.owner}</span>
-							</div>
-						</article>
-					);
-				})}
+				<Masonry elementType={'div'} options={{ transitionDuration: 0.5 }} disableImagesLoaded={false} updateOnEachImageLoad={false}>
+					{Pics.map((pic, idx) => {
+						return (
+							<article key={idx}>
+								<h2>{pic.title}</h2>
+								<div className='inner'>
+									<div className='pic'>
+										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+									</div>
+									<div className='profile'>
+										<img src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`} alt={pic.owner} />
+										<span>{pic.owner}</span>
+									</div>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
