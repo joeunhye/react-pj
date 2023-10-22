055c98df523d921a3628e6ffdfa1221b28a9e779
eunhye <masimel33@gmail.com>
2023-10-22 14:00:05 +0900

    타입에 따라 갤러리 호출 변환

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,26 +5,37 @@ import { useState, useEffect } from 'react';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
+	const myID = '199369997@N05';
 
-	const fetchGallery = async () => {
+	const fetchGallery = async (opt) => {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = process.env.REACT_APP_FLICKR_KEY;
-		const myID = '199369997@N05';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const num = 50;
+		let url = '';
 		const url_interest = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-		const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${myID}`;
-		const data = await fetch(url_user);
+		const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.id}`;
+
+		opt.type === 'user' && (url = url_user);
+		opt.type === 'interest' && (url = url_interest);
+
+		const data = await fetch(url);
 		const json = await data.json();
 		setPics(json.photos.photo);
 	};
 	useEffect(() => {
-		fetchGallery();
+		fetchGallery({ type: 'user', id: myID });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
+			<article className='controls'>
+				<nav className='btnSet'>
+					<button onClick={() => fetchGallery({ type: 'interest' })}>Interest Gallrey</button>
+					<button onClick={() => fetchGallery({ type: 'user', id: myID })}>My Gallrey</button>
+				</nav>
+			</article>
 			<div className='frame'>
 				<Masonry elementType={'div'} options={{ transitionDuration: 0.5 }} disableImagesLoaded={false} updateOnEachImageLoad={false}>
 					{Pics.map((pic, idx) => {
@@ -32,7 +43,7 @@ export default function Gallery() {
 							<article key={idx}>
 								<div className='inner'>
 									<div className='pic'>
-										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
+										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
 									</div>
 									<h2>{pic.title}</h2>
 
@@ -44,7 +55,7 @@ export default function Gallery() {
 												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
 											}}
 										/>
-										<span>{pic.owner}</span>
+										<span onClick={(e) => fetchGallery({ type: 'user', id: e.target.innerText })}>{pic.owner}</span>
 									</div>
 								</div>
 							</article>
