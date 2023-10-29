c8d82c18171677da9cfa2e249e91e169e1df5240
eunhye <masimel33@gmail.com>
2023-10-29 14:24:22 +0900

    커뮤니티 기능 추가

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -2,11 +2,30 @@ import './Community.scss';
 import Layout from '../../common/layout/Layout';
 import { TfiWrite } from 'react-icons/tfi';
 import { ImCancelCircle } from 'react-icons/im';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function Comunity() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const resetPost = () => {
+		refInput.current.value = '';
+		refTextarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!refInput.current.value.trim() || !refTextarea.current.value.trim()) {
+			resetPost();
+			return alert('제목과 본문을 모두 입력해주세요.');
+		}
+		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
+		resetPost();
+	};
+
+	useEffect(() => {
+		localStorage.setItem('posts', JSON.stringify(Posts));
+	}, [Posts]);
 
 	return (
 		<Layout title={'Community'}>
@@ -16,19 +35,51 @@ function Comunity() {
 					<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
 
 					<nav>
-						<button>
+						<button onClick={resetPost}>
 							<ImCancelCircle fontSize={20} color={'#555'} />
 						</button>
-						<button>
+						<button onClick={createPost}>
 							<TfiWrite fontSize={20} color={'#555'} />
 						</button>
 					</nav>
 				</div>
 
-				<div className='showBox'></div>
+				<div className='showBox'>
+					{Posts.map((post, idx) => (
+						<article key={idx}>
+							<div className='txt'>
+								<h2>{post.title}</h2>
+								<p>{post.content}</p>
+							</div>
+
+							<nav>
+								<button>Edit</button>
+								<button>Delete</button>
+							</nav>
+						</article>
+					))}
+				</div>
 			</div>
 		</Layout>
 	);
 }
 
 export default Comunity;
+
+/**
+ * Create : 글 작성 "POST"
+ * Read : 글 불러오기 "GET"
+ * Updated : 글 수정 "PUT"
+ * Delete: 글 삭제 "DELETE"
+ *
+ * RESTful API : DB를 구조적으로 변경하기 위한 개발 방법론
+ * 로컬 저장소(LocalStorage) :
+ * - 모든 브라우저가 내장하고 있는 경량의 저장공간
+ * - 문자값만 저장 가능
+ * - 객체값을 문자화(JSON.stringify) 시켜서 저장
+ * - 로컬 저장소 값을 가져올 때에는 JSON 형태로 객체로 파싱(JSON.parse)해서 가져옴
+ *
+ * LocalStorage 메서드
+ * 저장하기 : LocalStorage.setItem('키', '문자화된 데이터')
+ * 불러오기 : LocalStorage.getItem('키') : 문자값으로 불러오기 때문에 객체 형태로 파싱해야 한다.
+ */
