bdef2f679de6acff6fa66a93412f77557395e6dc
eunhye <masimel33@gmail.com>
2023-10-29 13:09:35 +0900

    커뮤니티 컴포넌트 스타일링

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,8 +1,34 @@
 import './Community.scss';
 import Layout from '../../common/layout/Layout';
+import { TfiWrite } from 'react-icons/tfi';
+import { ImCancelCircle } from 'react-icons/im';
+import { useRef } from 'react';
 
-function Community() {
-	return <Layout title={'Community'}>community</Layout>;
+function Comunity() {
+	const refInput = useRef(null);
+	const refTextarea = useRef(null);
+
+	return (
+		<Layout title={'Community'}>
+			<div className='wrap'>
+				<div className='inputBox'>
+					<input type='text' placeholder='title' ref={refInput} />
+					<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
+
+					<nav>
+						<button>
+							<ImCancelCircle fontSize={20} color={'#555'} />
+						</button>
+						<button>
+							<TfiWrite fontSize={20} color={'#555'} />
+						</button>
+					</nav>
+				</div>
+
+				<div className='showBox'></div>
+			</div>
+		</Layout>
+	);
 }
 
-export default Community;
+export default Comunity;
