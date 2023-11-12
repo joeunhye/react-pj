372431b28528fdb4abea61ba26d472e274f7dc79
eunhye <masimel33@gmail.com>
2023-11-12 12:53:12 +0900

    카카오맵 컨트롤러 추가

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,7 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Contact() {
 	const { kakao } = window;
@@ -50,12 +49,20 @@ export default function Contact() {
 
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
+		//지도 인스턴스 생성해서 지도화면 렌더링
 		mapInstance.current = new kakao.maps.Map(mapFrame.current, { center: info.current[Index].latlng });
+		//지도인스턴스에 맵타입 인스턴스로 타입컨트롤러 추가
+		mapInstance.current.addControl(new kakao.maps.MapTypeControl(), kakao.maps.ControlPosition.TOPRIGHT);
+		//지도인스턴스에 줌 인스턴스로 줌 컨트롤러 추가
+		mapInstance.current.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
+		//마커 인스턴에 맵 인스턴스 결해서 마커 출력
 		marker.setMap(mapInstance.current);
+
+		setTraffic(false);
 
 		window.addEventListener('resize', setCenter);
 	}, [Index]);
-  
+
 	//교통정보 보기 토글 기능
 	useEffect(() => {
 		Traffic
